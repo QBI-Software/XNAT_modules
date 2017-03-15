1c30fd34bf6d15b7a26b0f73ca06c869b6f87dc9
lizcw <e.cooperwilliams@uq.edu.au>
2017-03-15 16:45:46 +1000

    IRC5 samples plugin - all samples added

--- irc5-samples-plugin/src/main/java/qbi/xnat/irc5/samples/plugin/IRC5SamplesPlugin.java
+++ irc5-samples-plugin/src/main/java/qbi/xnat/irc5/samples/plugin/IRC5SamplesPlugin.java
@@ -3,18 +3,35 @@ package qbi.xnat.irc5.samples.plugin;
 import org.nrg.framework.annotations.XnatDataModel;
 import org.nrg.framework.annotations.XnatPlugin;
 import org.nrg.xdat.bean.Irc5BlooddataBean;
+import org.nrg.xdat.bean.Irc5BraindataBean;
+import org.nrg.xdat.bean.Irc5NpsdataBean;
+import org.nrg.xdat.bean.Irc5OpiniondataBean;
+import org.nrg.xdat.bean.Irc5SalivadataBean;
+import org.nrg.xdat.bean.Irc5SkindataBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @XnatPlugin(name="QBI IRC5 Bio samples", value="IRC5SamplesPlugin", description="QBI IRC5 Bio samples",
-		entityPackages = "qbi.xnat.irc5.blood.subjectmapping",
 dataModels = {@XnatDataModel(value = Irc5BlooddataBean.SCHEMA_ELEMENT_NAME,
                              singular = "Blood sample",
-                             plural = "Blood samples")})
-@ComponentScan(basePackages = {"qbi.xnat.irc5.samples.subjectmapping.preferences",
-        "qbi.xnat.irc5.samples.subjectmapping.repositories",
-        "qbi.xnat.irc5.samples.subjectmapping.rest",
-        "qbi.xnat.irc5.samples.subjectmapping.services.impl"})
+                             plural = "Blood samples"),
+		@XnatDataModel(value = Irc5BraindataBean.SCHEMA_ELEMENT_NAME,
+				singular = "Brain sample",
+				plural = "Brain samples"),
+		@XnatDataModel(value = Irc5NpsdataBean.SCHEMA_ELEMENT_NAME,
+				singular = "NeuroPsych scores",
+				plural = "NeuroPsych scores"),
+		@XnatDataModel(value = Irc5OpiniondataBean.SCHEMA_ELEMENT_NAME,
+				singular = "Medical Opinion",
+				plural = "Medical Opinions"),
+		@XnatDataModel(value = Irc5SalivadataBean.SCHEMA_ELEMENT_NAME,
+				singular = "Saliva sample",
+				plural = "Saliva samples"),
+		@XnatDataModel(value = Irc5SkindataBean.SCHEMA_ELEMENT_NAME,
+				singular = "Skin sample",
+				plural = "Skin samples"),
+}
+                             )
 
 public class IRC5SamplesPlugin {
 	 @Bean
