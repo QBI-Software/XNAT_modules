ff548c7afb682b5059e7b718a1557822cd45bc68
lizcw <e.cooperwilliams@uq.edu.au>
2017-06-30 18:52:25 +1000

    IRC5 javascript issues; OPEX adding all fields

--- irc5-samples-plugin/src/main/java/qbi/xnat/irc5/samples/plugin/IRC5SamplesPlugin.java
+++ irc5-samples-plugin/src/main/java/qbi/xnat/irc5/samples/plugin/IRC5SamplesPlugin.java
@@ -12,31 +12,38 @@ import org.nrg.xdat.bean.Irc5ImageanalysisdataBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-@XnatPlugin(name="IRC5 Bio samples", value="IRC5SamplesPlugin", description="IRC5 Bio samples",
-dataModels = {@XnatDataModel(value = Irc5BlooddataBean.SCHEMA_ELEMENT_NAME,
+@XnatPlugin(name="IRC5 Bio samples", value="IRC5SamplesPlugin",
+		description="IRC5 Bio samples",
+		entityPackages = "qbi.xnat.irc5.samples.subjectmapping.entities",
+		dataModels = {@XnatDataModel(value = Irc5BlooddataBean.SCHEMA_ELEMENT_NAME,
                              singular = "Blood sample",
-                             plural = "Blood samples"),
+                             plural = "Blood samples",
+							 code = "BLO"),
 		@XnatDataModel(value = Irc5BraindataBean.SCHEMA_ELEMENT_NAME,
 				singular = "Brain sample",
-				plural = "Brain samples"),
+				plural = "Brain samples",
+				code = "BRA"),
 		@XnatDataModel(value = Irc5NpsdataBean.SCHEMA_ELEMENT_NAME,
 				singular = "NeuroPsych scores",
-				plural = "NeuroPsych scores"),
+				plural = "NeuroPsych scores",
+				code = "NPS"),
 		@XnatDataModel(value = Irc5OpiniondataBean.SCHEMA_ELEMENT_NAME,
 				singular = "Medical Opinion",
-				plural = "Medical Opinions"),
+				plural = "Medical Opinions",
+				code = "MO"),
 		@XnatDataModel(value = Irc5SalivadataBean.SCHEMA_ELEMENT_NAME,
 				singular = "Saliva sample",
-				plural = "Saliva samples"),
+				plural = "Saliva samples",
+				code = "SAL"),
 		@XnatDataModel(value = Irc5SkindataBean.SCHEMA_ELEMENT_NAME,
 				singular = "Skin sample",
-				plural = "Skin samples"),
+				plural = "Skin samples",
+				code = "SK"),
 		@XnatDataModel(value = Irc5ImageanalysisdataBean.SCHEMA_ELEMENT_NAME,
 				singular = "Image analysis",
-				plural = "Image analysis"),
-}
-                             )
-
+				plural = "Image analysis",
+				code = "IA")
+} )
 public class IRC5SamplesPlugin {
 	 @Bean
 	 public String IRC5SamplesPluginMessage() {
